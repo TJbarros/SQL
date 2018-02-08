aa89ac943e51492125576294e9eca830f1f909ed
Thiago Julierme A. Barros <tjabarros@hotmail.com>
2018-02-08 11:12:28 -0200

    Update replace.sql

--- replace.sql
+++ replace.sql
@@ -1,2 +1,3 @@
-﻿UPDATE tabela
+-- Substituir registro na tabela usando replace função nativa do MySQL - REPLACE() junto com o comando de UPDATE. --
+UPDATE tabela
 SET coluna = REPLACE(coluna, 'RegistroAntigo', ‘NovoRegistro’);
